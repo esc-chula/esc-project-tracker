5a40e3333f70bd3a4f8b4a21870c33ec0054018f
LittleNa1000 <naphanccnp@gmail.com>
2024-05-17 04:07:20 +0700

    chore: color palette

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,25 @@
 // tailwind config is required for editor support
 
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss"
+import sharedConfig from "@repo/tailwind-config"
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets" | "theme"> = {
   content: ["./src/app/**/*.tsx"],
   presets: [sharedConfig],
-};
+  theme: {
+    extend: {
+      colors: {
+        intania: "#5E1018",
+        red: "#C22231",
+        lightpink: "#FFF0F0",
+        pink: "#E59DA4",
+      },
+      backgroundImage: {
+        "gradient-red": "linear-gradient(180deg, #5E1018 0%, #C22231 100%)",
+        "gradient-pink": "linear-gradient(180deg, #E59DA4 0%, #FFF0F0 100%)",
+      },
+    },
+  },
+}
 
-export default config;
+export default config
