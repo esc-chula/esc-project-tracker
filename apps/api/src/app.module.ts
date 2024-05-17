8f35d94fdb08f59831a8373d2a0ce5d43561c113
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-05-17 22:25:21 +0700

    chore : init TypeORM

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          url: configService.get('DATABASE_URL'),
+          entities: [join(__dirname, '**/*.entity.{ts,js}')],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
+    }),
+
+    TrpcModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
