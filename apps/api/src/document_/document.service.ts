13e0fa7fed10130df03582931a46a925de256f34
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-05-19 02:41:06 +0700

    chore: basic services

--- apps/api/src/document_/document.service.ts
+++ apps/api/src/document_/document.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Document } from '../entities/document.entity';
 import { Repository } from 'typeorm';
+import { Project } from '../entities/project.entity';
+import { UserService } from '../user_/user.service';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class DocumentService {
   constructor(
     @InjectRepository(Document)
     private readonly documentRepository: Repository<Document>,
+    private readonly userService: UserService,
   ) {}
 
   findByDocID(docID: string): Promise<Document> | undefined {
