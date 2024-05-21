a0b4c8e68639683b2095ebb6a5a823813a0787c9
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-05-22 05:25:19 +0700

    add parameter to lastestItem

--- apps/web/src/components/my-projects/lastest-item.tsx
+++ apps/web/src/components/my-projects/lastest-item.tsx
@@ -1,15 +1,21 @@
 import { File } from "lucide-react";
 
-export default function LastestItem() {
+export default function LastestItem({
+  projectCode,
+  projectName,
+}: {
+  projectCode: string;
+  projectName: string;
+}) {
   return (
-    <div className="bg-card text-card-foreground rounded-md flex flex-row py-2 px-10 space-x-5 border-2 border-black">
+    <div className="bg-card text-card-foreground rounded-md flex flex-row py-2 px-8 space-x-5 border-2 border-black">
       <div className="flex items-center justify-center">
         <File size={40} strokeWidth={1} />
       </div>
-      <div className="flex flex-col text-primary">
-        <div className="font-semibold text-2xl">9090</div>
+      <div className="font-semibold text-2xl text-foreground">
+        {projectCode}
         <div className="text-xs font-semibold text-ellipsis overflow-hidden whitespace-nowrap max-w-20">
-          โครงกาcascsacascascascacacacacs
+          {projectName}
         </div>
       </div>
     </div>
--- apps/web/src/components/my-projects/lastest-panel.tsx
+++ apps/web/src/components/my-projects/lastest-panel.tsx
@@ -1,16 +1,17 @@
-import Subtitle from "../header/Subtitle";
-import Title from "../header/Title";
 import LastestItem from "./lastest-item";
 
 export default function LastestPanel() {
   return (
     <div className="flex flex-col">
-      <div>ล่าสุด</div>
+      <div className="text-black font-bold">ล่าสุด</div>
       <div className="bg-[#D9D9D9] bg-opacity-20 flex flex-row justify-between p-5 rounded-lg">
-        <LastestItem />
-        <LastestItem />
-        <LastestItem />
-        <LastestItem />
+        <LastestItem projectCode="8888" projectName="โครงการอบรมเด็กยากไร้" />
+        <LastestItem
+          projectCode="5555"
+          projectName="โครงการอบบมอิอิอิอิิอิอิอิอิ"
+        />
+        <LastestItem projectCode="4444" projectName="โครงการแฟหแหฟฟหแ" />
+        <LastestItem projectCode="1111" projectName="cascascascasc" />
       </div>
     </div>
   );
