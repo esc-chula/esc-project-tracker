d15e684358bc2f902296433423658dfd6650476e
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-05-22 14:30:43 +0700

    feat: add carousal

--- apps/web/src/components/my-projects/lastest-item.tsx
+++ apps/web/src/components/my-projects/lastest-item.tsx
@@ -10,9 +10,9 @@ export default function LastestItem({
   return (
     <div className="bg-card text-card-foreground rounded-md flex flex-row py-2 px-8 space-x-5 border-2 border-black">
       <div className="flex items-center justify-center">
-        <File size={40} strokeWidth={1} />
+        <File size={30} strokeWidth={1} />
       </div>
-      <div className="font-semibold text-2xl text-foreground">
+      <div className="font-semibold text-xl text-foreground">
         {projectCode}
         <div className="text-xs font-semibold text-ellipsis overflow-hidden whitespace-nowrap max-w-20">
           {projectName}
--- apps/web/src/components/my-projects/lastest-panel.tsx
+++ apps/web/src/components/my-projects/lastest-panel.tsx
@@ -1,17 +1,34 @@
+"use client";
 import LastestItem from "./lastest-item";
+import useEmblaCarousel from "embla-carousel-react";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
 
 export default function LastestPanel() {
+  const [carouselRef] = useEmblaCarousel({ loop: false, dragFree: true }, [
+    WheelGesturesPlugin(),
+  ]);
+
   return (
     <div className="flex flex-col">
       <div className="text-black font-bold">ล่าสุด</div>
-      <div className="bg-[#D9D9D9] bg-opacity-20 flex flex-row justify-between p-5 rounded-lg">
-        <LastestItem projectCode="8888" projectName="โครงการอบรมเด็กยากไร้" />
-        <LastestItem
-          projectCode="5555"
-          projectName="โครงการอบบมอิอิอิอิิอิอิอิอิ"
-        />
-        <LastestItem projectCode="4444" projectName="โครงการแฟหแหฟฟหแ" />
-        <LastestItem projectCode="1111" projectName="cascascascasc" />
+      <div
+        className="bg-[#D9D9D9] bg-opacity-20 p-5 rounded-lg overflow-hidden w-[55vw]"
+        ref={carouselRef}
+      >
+        <div className="flex space-x-10">
+          <LastestItem projectCode="8888" projectName="โครงการอบรมเด็กยากไร้" />
+          <LastestItem
+            projectCode="5555"
+            projectName="โครงการอบบมอิอิอิอิิอิอิอิอิ"
+          />
+          <LastestItem projectCode="4444" projectName="โครงการแฟหแหฟฟหแ" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+          <LastestItem projectCode="1111" projectName="cascascascasc" />
+        </div>
       </div>
     </div>
   );
