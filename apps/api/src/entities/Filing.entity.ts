fdffd4ba7df11f9dc9b74722b516d0b84d70734a
ReawEiEi <chot.fueng@gmail.com>
2024-05-23 15:28:19 +0700

    fix: db-schema and enum(status)

--- apps/api/src/entities/Filing.entity.ts
+++ apps/api/src/entities/Filing.entity.ts
@@ -1,33 +1,39 @@
-import { Column, CreateDateColumn, Entity, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Project } from "./Project.entity";
-import { DocStatus } from "../constant/enum";
-import { User } from "./User.entity";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Project } from './Project.entity';
+import { FilingStatus } from '../constant/enum';
+import { User } from './User.entity';
 
 @Entity()
 export class Filing {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @OneToOne(() => Project, { onDelete: 'CASCADE' })
-    project: Project
+  @ManyToOne(() => Project, { onDelete: 'CASCADE' })
+  project: Project;
 
-    @OneToOne(() => User, { onDelete: 'CASCADE' })
-    user: User
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  user: User;
 
-    @Column(
-        {
-            type: 'enum',
-            enum: DocStatus
-        }
-    )
-    status: DocStatus
+  @Column({
+    type: 'enum',
+    enum: FilingStatus,
+  })
+  status: FilingStatus;
 
-    @Column()
-    projectCode: string;
+  @Column()
+  projectCode: string;
 
-    @CreateDateColumn()
-    createdAt: Date
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn({ nullable: true, default: null })
-    updatedAt: Date
+  @UpdateDateColumn({ nullable: true, default: null })
+  updatedAt: Date;
 }
