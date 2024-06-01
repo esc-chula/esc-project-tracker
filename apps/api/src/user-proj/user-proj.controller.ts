f1ddff8d91a4dc90b4a95341fa5e9d658a7cf5d4
ReawEiEi <chot.fueng@gmail.com>
2024-06-01 15:41:56 +0700

    add sevice and test api

--- apps/api/src/user-proj/user-proj.controller.ts
+++ apps/api/src/user-proj/user-proj.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserProjService } from './user-proj.service';
+import { CreateUserProjDTO } from './user-project.dto';
 
 @Controller('user-proj')
-export class UserProjController {}
+export class UserProjController {
+  constructor(private readonly userProjService: UserProjService) {}
+
+  // TEST API
+  @Post('userJoinProject')
+  createUserProject(@Body() obj: CreateUserProjDTO) {
+    console.log('UserProjController.createUserProject', obj);
+    return this.userProjService.createUserProject({
+      obj,
+    });
+  }
+}
---
+++ apps/api/src/user-proj/user-project.dto.ts
@@ -0,0 +1,6 @@
+import { UserProj } from '../entities/userProj.entity';
+
+export class CreateUserProjDTO {
+  userId: string;
+  projectId: string;
+}
