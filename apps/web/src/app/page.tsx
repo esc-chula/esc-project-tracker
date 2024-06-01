25cbdf516e1d93bd3c577958d6c91c6683b40569
ReawEiEi <chot.fueng@gmail.com>
2024-06-01 20:51:23 +0700

    fix web to default

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,6 @@
 import { trpc } from "./trpc";
 
 export default function Home(): JSX.Element {
-  const data = trpc.deleteUserProject.query({
-    userId: "5c1529a9-046a-4f85-a11a-d269dedf4e20",
-    projectId: "7d80bb7b-79f4-4c54-b9c9-fc39e2070c55",
-  });
   return (
     <div className="flex flex-wrap ml-9 my-5">
       <div className="w-full max-w-full sm:w-1/4 mx-auto text-center">
