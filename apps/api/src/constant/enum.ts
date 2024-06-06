8dcd1fa4d5a7773ea9263e61421af0e5c7e537c6
ReawEiEi <chot.fueng@gmail.com>
2024-06-06 22:50:24 +0700

    fix:enum code type

--- apps/api/src/constant/enum.ts
+++ apps/api/src/constant/enum.ts
@@ -15,16 +15,17 @@ export enum ProjectStatus {
 }
 
 export enum ProjectType {
-  INTERNAL_AFFAIR = '10',
-  ARTS_CULTURE_INTERNAL_AFFAIR = '11',
-  ACADEMICS_INTERNAL_AFFAIR = '12',
-  SPORTS_INTERNAL_AFFAIR = '13',
-  SOCIAL_SERVICE_INTERNAL_AFFAIR = '14',
-  NISITSUMPAN_INTERNAL_AFFAIR = '15',
-  EXTERNAL_AFFAIR = '20',
-  INTERNATIONAL_ACTIVITIES = '30',
-  ORGANIZATION_DEVELOPMENT = '40',
-  SUPPORT = '50',
-  CORPORATE_COMMUNICATION = '60',
-  OTHER_ESC = '70',
+  INTERNAL_AFFAIR = '10', // กิจการภายใน
+  ARTS_CULTURE_AFFAIR = '11', // ศิลปะและวัฒนธรรม
+  SPORTS_AFFAIR = '12', // กีฬา
+  SOCIAL_SERVICE_AFFAIR = '13', // พัฒนาสังคมและบำเพ็ญประโยชน์
+  STUDENTS_WELFARE_ENV_AFFAIR = '14', // สวัสดิการนิสิตและสิ่งแวดล้อม
+  EXTERNAL_AFFAIR = '20', // กิจการภายนอก
+  NISITSUMPAN_AFFAIR = '30', // นิสิตสัมพันธ์
+  TECH_AFFAIR = '40', //เทคโนโลยี
+  ORGANIZATION_AFFAIR = '50', // พัฒนาองค์กร
+  PR_MARGETING_AFFAIR = '60', // ประชาสัมพันธ์และการตลาด
+  ACADEMICS_AFFAIR = '70', // วิชาการ
+  OTHER_ESC = '80', // อื่นๆของกวศ
+  OFFICE_SUPPLY_AFFAIR = '90', // สำนักงานและพัสดุ
 }
