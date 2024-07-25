344b0db09f8272eeb3c34f44970c48e6bcf283f5
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-07-25 21:52:15 +0700

    feat: project info component
    add todo

--- apps/web/src/components/all-projects/addNewProjectButton.tsx
+++ apps/web/src/components/all-projects/addNewProjectButton.tsx
@@ -1,6 +1,6 @@
-"use client";
-import { FaFolderPlus } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+'use client';
+import { FaFolderPlus } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 
 export default function AddNewProjectButton() {
   const router = useRouter();
@@ -8,7 +8,7 @@ export default function AddNewProjectButton() {
     <button
       className="bg-red text-foreground text-white px-4 py-2 rounded-lg"
       onClick={() => {
-         router.push("/new-project")
+        router.push('new-project');
       }}
     >
       <FaFolderPlus className="inline-block mr-3" />
