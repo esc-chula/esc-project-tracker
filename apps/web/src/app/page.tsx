a0bc342d3458cbf4959d5aec579fecfe8b2b98bc
LittleNa1000 <naphanccnp@gmail.com>
2024-09-20 17:32:38 +0700

    styling: landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -8,18 +8,20 @@ export default function Page() {
     <>
       <main className="flex flex-col items-center">
         <OnboardingPageHeader showAdminPortal />
-        <Image
-          alt="main-logo"
-          src="/main-logo.webp"
-          width={566}
-          height={564}
-          className="w-[35vh] mt-10 mb-6 aspect-auto"
-        />
-        <h1 className="text-5xl font-bold mb-4">Document System</h1>
-        <h2 className="text-2xl mb-6">
-          ดำเนินการเรื่องเอกสารของคณะฯ ไม่ยุ่งยาก สะดวก รวดเร็ว
-          ครบจบทุกกระบวนการ มาเริ่มต้นเอกสารกัน !
-        </h2>
+        <div className="mx-8 flex flex-col items-center">
+          <Image
+            alt="main-logo"
+            src="/main-logo.webp"
+            width={566}
+            height={564}
+            className="w-[35vh] mt-10 mb-6 aspect-auto"
+          />
+          <h1 className="text-5xl font-bold mb-4">Document System</h1>
+          <h2 className="text-2xl mb-6 text-center">
+            ดำเนินการเรื่องเอกสารของคณะฯ ไม่ยุ่งยาก สะดวก รวดเร็ว
+            ครบจบทุกกระบวนการ มาเริ่มต้นเอกสารกัน !
+          </h2>
+        </div>
 
         <Button
           className="rounded-[80px] bg-gradient-red text-2xl font-bold px-12 py-[10px] h-16"
