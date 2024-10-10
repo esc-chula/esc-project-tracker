abf1c575eb31807a75781553dd112f5f5af8bdac
LittleNa1000 <naphanccnp@gmail.com>
2024-10-10 19:27:40 +0700

    fix last commit

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,10 +1,10 @@
 import LoginButton from '@/src/components/common/login-button';
-import LandingPageHeader from '@/src/components/header/LandingPageHeader';
+import OnboardingPageHeader from '@/src/components/header/OnboardingPageHeader';
 
 export default function Page() {
   return (
     <main className="flex flex-col py-10 px-4 items-center w-full gap-10">
-      <LandingPageHeader />
+      <OnboardingPageHeader />
       <div className="flex flex-col items-center">
         <LoginButton />
       </div>
