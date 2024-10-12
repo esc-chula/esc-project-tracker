fcb781255925993600404742e426ce713809b4ca
ReawEiEi <chot.fueng@gmail.com>
2024-10-12 17:36:06 +0700

    authorize docs

--- apps/api/src/trpc/routers/document.router.ts
+++ apps/api/src/trpc/routers/document.router.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { TrpcService } from '../trpc.service';
-
 import { optional, z } from 'zod';
-
 import { DocumentService } from '../../document_/document.service';
 import { DocumentActivity, DocumentStatus } from '../../constant/enum';
-import { find } from 'rxjs';
 import { TRPCError } from '@trpc/server';
 
 @Injectable()
@@ -16,44 +13,38 @@ export class DocumentRouter {
   ) {}
 
   appRouter = this.trpcService.router({
-    // Get Documents By UserID -> Document[]
-    findDocumentsByUserId: this.trpcService.trpc.procedure
-      .input(z.object({ userId: z.string() }))
-      .query(({ input }) => {
-        return this.documentService.findByUserID(input.userId);
-      }),
-    // Get Documents By ProjectID -> Document[]
-    findDocumentsByProjectId: this.trpcService.trpc.procedure
-      .input(z.object({ projectId: z.string() }))
-      .query(({ input }) => {
-        return this.documentService.findByProjectID(input.projectId);
-      }),
-
     //get Documents by filingId
-    findDocumentsByFilingId: this.trpcService.trpc.procedure
+    findDocumentsByFilingId: this.trpcService.protectedProcedure
       .input(z.object({ filingId: z.string() }))
       .query(({ input }) => {
         return this.documentService.findDocumentsByFilingId(input.filingId);
       }),
-    findLatestDocumentByFilingId: this.trpcService.trpc.procedure
+    findLatestDocumentByFilingId: this.trpcService.protectedProcedure
       .input(z.object({ filingId: z.string().uuid() }))
       .query(({ input }) => {
         return this.documentService.findLatestDocumentByFilingId(
           input.filingId,
         );
       }),
-      findLatestReplyByFilingId: this.trpcService.trpc.procedure.input(z.object({ filingId: z.string() })).query(({ input }) => {
-        return this.documentService.findLatestReplyDocumentByFilingId(input.filingId);
-      }
-      ),
-
-      findLatestPendingByFilingId: this.trpcService.trpc.procedure.input(z.object({ filingId: z.string() })).query(({ input }) => {
-        return this.documentService.findLatestPendingDocumentByFilingId(input.filingId);
-      }
-      ),
+    // Used in admin section Only for now
+    findLatestReplyByFilingId: this.trpcService.adminProcedure
+      .input(z.object({ filingId: z.string() }))
+      .query(({ input }) => {
+        return this.documentService.findLatestReplyDocumentByFilingId(
+          input.filingId,
+        );
+      }),
+    // Used in admin section Only for now
+    findLatestPendingByFilingId: this.trpcService.adminProcedure
+      .input(z.object({ filingId: z.string() }))
+      .query(({ input }) => {
+        return this.documentService.findLatestPendingDocumentByFilingId(
+          input.filingId,
+        );
+      }),
 
     // Create Document -> Document
-    createDocument: this.trpcService.trpc.procedure
+    createDocument: this.trpcService.protectedProcedure
       .input(
         z.object({
           filingId: z.string(),
@@ -82,7 +73,7 @@ export class DocumentRouter {
       }),
 
     //edit document
-    updateDocument: this.trpcService.trpc.procedure
+    updateDocument: this.trpcService.protectedProcedure
       .input(
         z.object({
           docId: z.string(),
@@ -117,7 +108,9 @@ export class DocumentRouter {
           });
         return this.documentService.deleteDocument(input.id);
       }),
-    reviewSubmission: this.trpcService.trpc.procedure
+
+    // Used in admin section Only for now
+    reviewSubmission: this.trpcService.adminProcedure
       .input(z.object({ id: z.string().uuid(), updatedStatus: z.boolean() }))
       .mutation(({ input }) => {
         return this.documentService.reviewDocumentSubmission(
