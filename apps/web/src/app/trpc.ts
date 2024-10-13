8858e16cb599807759e1b02f4c952975ebe24009
LittleNa1000 <naphanccnp@gmail.com>
2024-10-13 21:06:07 +0700

    fix import, correct .env.example

--- apps/web/src/app/trpc.ts
+++ apps/web/src/app/trpc.ts
@@ -1,10 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import { AppRouter } from 'api/src/trpc/trpc.router';
+import type { AppRouter } from '../../../api/src/trpc/trpc.router';
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url:  `${process.env.NEXT_PUBLIC_API_SERVER_URL}/trpc`,
+      url: `${process.env.NEXT_PUBLIC_API_SERVER_URL}/trpc`,
       fetch(url, options) {
         return fetch(url, {
           ...options,
