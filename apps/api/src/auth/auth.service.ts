2badec7335c0b11ed384e9f04d7a343cbb530d96
LittleNa1000 <naphanccnp@gmail.com>
2024-10-22 16:16:22 +0700

    fix build error

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -87,7 +87,7 @@ export class AuthService {
 
   async signOut(accessToken: string) {
     const payload = await this.validateJWT(accessToken);
-    await this.userService.update(payload.sub, { refreshToken: null });
+    await this.userService.update(payload.sub, { refreshToken: undefined });
   }
 
   async me(userId: string) {
