436296e703365dc649b1f821779e114dbcf4feb3
LittleNa1000 <naphanccnp@gmail.com>
2024-10-31 01:21:34 +0700

    skip env validation

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -11,15 +11,16 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_SERVER_URL: z.string().url(),
   },
-  runtimeEnv: {
-    INTANIA_AUTH_APP_ID: process.env.INTANIA_AUTH_APP_ID,
-    INTANIA_AUTH_REDIRECT_URL: process.env.INTANIA_AUTH_REDIRECT_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
-    NEXT_PUBLIC_API_SERVER_URL: process.env.NEXT_PUBLIC_API_SERVER_URL,
-    NODE_ENV: process.env.NODE_ENV,
-  },
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  // experimental__runtimeEnv: {
+  // runtimeEnv: {
+  //   INTANIA_AUTH_APP_ID: process.env.INTANIA_AUTH_APP_ID,
+  //   INTANIA_AUTH_REDIRECT_URL: process.env.INTANIA_AUTH_REDIRECT_URL,
+  //   JWT_SECRET: process.env.JWT_SECRET,
   //   NEXT_PUBLIC_API_SERVER_URL: process.env.NEXT_PUBLIC_API_SERVER_URL,
+  //   NODE_ENV: process.env.NODE_ENV,
   // },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_API_SERVER_URL: process.env.NEXT_PUBLIC_API_SERVER_URL,
+  },
+  skipValidation: true,
 });
