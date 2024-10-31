5bd16b70c850183cb5b67338fd1021b7252e8a42
LittleNa1000 <naphanccnp@gmail.com>
2024-10-31 12:42:04 +0700

    /api synchronize: true

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return `Hello World! ${process.env.REGION} ${this.configService.get<string>('REGION')}`;
+    return 'Hello World!';
   }
 }
