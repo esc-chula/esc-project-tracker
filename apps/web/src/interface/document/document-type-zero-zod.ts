2ab00f64e2157a86a6df3c39c47e9e98aae2cdc9
Yiwah <patsaporn.kaewkong@gmail.com>
2025-02-23 13:41:54 +0700

    refactor: turn into camelCase

--- apps/web/src/interface/document/document-type-zero-zod.ts
+++ apps/web/src/interface/document/document-type-zero-zod.ts
@@ -1,21 +1,22 @@
 import { z } from "zod";
-import { ObjectiveType, ActivityType, GraduateAttributeType, FilingType, ManagementRole, TQFStandardType, SustainableDevelopmentGoalType } from "@/src/constant/enum";
+import { ObjectiveType, ActivityType, GraduateAttributeType, FilingType, ManagementRole, TQFType, SDGType, PlanPhase } from "@/src/constant/enum";
 
 export const ObjectiveSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   percent: z.number(),
   type: z.nativeEnum(ObjectiveType),
   detail: z.string(),
 });
 
 export const IndicatorSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   topic: z.string(),
   measurement: z.string(),
-  related_objective: z.array(z.string()),
+  relatedObjective: z.array(z.string()),
 });
 
 export const ParticipantSchema = z.object({
+  id: z.string().readonly(),
   role: z.enum(["Worker", "Participant"]),
   year1: z.number(),
   year2: z.number(),
@@ -26,133 +27,134 @@ export const ParticipantSchema = z.object({
 });
 
 export const ActivityFormatSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   step: z.number(),
   topic: z.string(),
   detail: z.string(),
 });
 
 export const MemberSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   role: z.nativeEnum(ManagementRole),
   division: z.string().optional(),
   name: z.string(),
-  student_id: z.string(),
+  studentId: z.string(),
   tel: z.string(),
 });
 
 export const ImprovementItemSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   problem: z.string(),
   solution: z.string(),
 });
 
 export const ImprovementPlanSchema = z.object({
-  id: z.string(),
-  phase: z.enum(["Preparation", "Execution", "Conclusion"]),
+  id: z.string().readonly(),
+  phase: z.nativeEnum(PlanPhase),
   details: z.array(ImprovementItemSchema),
 });
 
 export const ExpectedOutcomesSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   category: z.enum(["Participant", "Worker", "Faculty/University"]),
   detail: z.array(z.string()),
 });
 
 export const WorkPlanItemSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   detail: z.string(),
-  start_date: z.string(),
-  end_date: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
 });
 
 export const WorkPlanSchema = z.object({
-  id: z.string(),
-  phase: z.enum(["Preparation", "Execution", "Conclusion"]),
+  id: z.string().readonly(),
+  phase: z.nativeEnum(PlanPhase),
   details: z.array(WorkPlanItemSchema),
 });
 
-const BaseBudgetDetailSchema = z.object({
-  id: z.string(),
-  type: z.enum(["Type 1", "Type 2"]),
+export const BudgetType1Schema = z.object({
+  id: z.string().readonly(),
+  type: z.literal("Type 1"),
   name: z.string(),
   amount: z.number(),
   unit: z.string(),
-  cost_per_unit: z.number(),
+  costPerUnit: z.number(),
 });
 
-export const BudgetType1Schema = BaseBudgetDetailSchema.extend({
-  type: z.literal("Type 1"),
-});
-
-export const BudgetType2Schema = BaseBudgetDetailSchema.extend({
+export const BudgetType2Schema = z.object({
+  id: z.string().readonly(),
   type: z.literal("Type 2"),
-  number_of_days: z.number(),
-  day_unit: z.string(),
+  name: z.string(),
+  amount: z.number(),
+  unit: z.string(),
+  costPerUnit: z.number(),
+  numberOfDays: z.number(),
+  dayUnit: z.string(),
 });
 
 export const BudgetSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   source: z.enum(["Faculty", "Sponsor", "Other"]),
   category: z.enum(["Material", "Expense"]),
-  details: z.array(z.union([BudgetType1Schema, BudgetType2Schema])),
+  details: z.array(z.discriminatedUnion("type", [BudgetType1Schema, BudgetType2Schema])),
 });
 
 export const GraduateAttributeSchema = z.object({
-  id: z.string(),
+  id: z.string().readonly(),
   attribute: z.nativeEnum(GraduateAttributeType),
   question: z.nativeEnum(ObjectiveType),
-  response_leader: z.boolean(),
-  response_worker: z.boolean(),
-  response_attendee: z.boolean(),
-  activity_id: z.array(z.string()),
+  responseLeader: z.boolean(),
+  responseWorker: z.boolean(),
+  responseAttendee: z.boolean(),
+  activityId: z.array(z.string()),
 });
 
-export const SustainableDevelopmentGoalSchema = z.object({
-  id: z.string(),
-  sdgs_goal: z.nativeEnum(SustainableDevelopmentGoalType),
-  activity_id: z.array(z.string()),
+export const SDGSchema = z.object({
+  id: z.string().readonly(),
+  sdgGoal: z.nativeEnum(SDGType),
+  activityId: z.array(z.string()),
 });
 
-export const TQFStandardSchema = z.object({
-  id: z.string(),
-  tqf_standards: z.nativeEnum(TQFStandardType),
-  activity_id: z.array(z.string()),
+export const TQFSchema = z.object({
+  id: z.string().readonly(),
+  tqfStandard: z.nativeEnum(TQFType),
+  activityId: z.array(z.string()),
 });
 
 export const DocumentTypeZeroSchema = z.object({
-  id: z.string(),
-  project_code: z.string(),
-  project_name_th: z.string(),
-  project_name_en: z.string(),
-  filing_type: z.nativeEnum(FilingType),
-  filing_code: z.string(),
-  filing_name: z.string(),
+  id: z.string().readonly(),
+  projectCode: z.string(),
+  projectNameTh: z.string(),
+  projectNameEn: z.string(),
+  filingType: z.nativeEnum(FilingType),
+  filingCode: z.string(),
+  filingName: z.string(),
   department: z.string().nullable(),
   isOneDay: z.boolean(),
-  start_date: z.string(),
-  end_date: z.string().optional(),
-  principle_and_rational_detail: z.string(),
-  objective_summarize_detail: z.string(),
+  startDate: z.string(),
+  endDate: z.string().optional(),
+  principleAndRationalDetail: z.string(),
+  objectiveSummarizeDetail: z.string(),
   objectives: z.array(ObjectiveSchema),
   indicators: z.array(IndicatorSchema),
-  approximate_participants: ParticipantSchema,
-  approximate_workers: ParticipantSchema,
-  work_place: z.string(),
-  activity_format: z.array(ActivityFormatSchema),
-  member: z.array(MemberSchema),
-  improvement_plans: z.array(ImprovementPlanSchema),
-  expected_outcomes: z.array(ExpectedOutcomesSchema),
-  work_plans: z.array(WorkPlanSchema),
-  actual_work_hours: z.number(),
-  actual_volunteer_hours: z.number(),
+  approximateParticipants: ParticipantSchema,
+  approximateWorkers: ParticipantSchema,
+  workPlace: z.string(),
+  activityFormat: z.array(ActivityFormatSchema),
+  members: z.array(MemberSchema),
+  improvementPlans: z.array(ImprovementPlanSchema),
+  expectedOutcomes: z.array(ExpectedOutcomesSchema),
+  workPlans: z.array(WorkPlanSchema),
+  actualWorkHours: z.number(),
+  actualVolunteerHours: z.number(),
   budgets: z.array(BudgetSchema),
-  graduate_attributes: z.array(GraduateAttributeSchema),
-  type_of_activity: z.nativeEnum(ActivityType),
-  sdgs: z.array(SustainableDevelopmentGoalSchema),
-  tqf_standards: z.array(TQFStandardSchema),
-  created_by: z.string(),
-  updated_by: z.string(),
+  graduateAttributes: z.array(GraduateAttributeSchema),
+  typeOfActivity: z.nativeEnum(ActivityType),
+  sdgs: z.array(SDGSchema),
+  tqfStandards: z.array(TQFSchema),
+  createdBy: z.string(),
+  updatedBy: z.string(),
   createdAt: z.string(),
   updatedAt: z.string().optional(),
 });
--- apps/web/src/interface/document/document-type-zero.ts
+++ apps/web/src/interface/document/document-type-zero.ts
@@ -1,39 +1,39 @@
-import type { ObjectiveType, ActivityType, GraduateAttributeType, FilingType, ManagementRole, TQFStandardType, SustainableDevelopmentGoalType } from "@/src/constant/enum";
+import type { ObjectiveType, ActivityType, GraduateAttributeType, FilingType, ManagementRole, TQFType, SDGType, PlanPhase } from "@/src/constant/enum";
 
 // design by following figma: https://www.figma.com/design/Dz6HOGk6pFbHFLdsra05Fh/ESC-Project-Tracker--%5BPhase-2%5D?node-id=0-1&p=f&t=fT3aiSmCE6oX1NMj-0
 export interface DocumentTypeZero { // เอกสารเปิดโครง
   readonly id: string;
-  project_code: string;
-  project_name_th: string;
-  project_name_en: string;
-  filing_type: FilingType;
-  filing_code: string;
-  filing_name: string;
+  projectCode: string;
+  projectNameTh: string;
+  projectNameEn: string;
+  filingType: FilingType;
+  filingCode: string;
+  filingName: string;
   department: string | null; // esc fields, club, null
   isOneDay: boolean;
-  start_date: string;
-  end_date?: string; // for project has more than 1 days
-  principle_and_rational_detail: string; //หลักการและเหตุผล
-  objective_summarize_detail: string; // วัตถุประสงค์โครงการแบบย่อ
+  startDate: string;
+  endDate?: string; // For projects lasting more than 1 day
+  principleAndRationalDetail: string; // หลักการและเหตุ
+  objectiveSummarizeDetail: string; // วัตถุประสงค์โครงการแบบย่อ
   objectives: Objective[]; // วัตถุประสงค์โครงการ
   indicators: Indicator[]; // ตัวชี้วัดความสำเร็จ
-  approximate_participants: Participant; // จำนวนผู้เข้าร่วมที่คาดว่าจะเข้าร่วมโครงการ
-  approximate_workers: Participant; // จำนวนผู้ปฏิบัติงานที่คาดว่าจะเข้าร่วมโครงการ
-  work_place: string;
-  activity_format: ActivityFormat[]; // รูปแบบกิจกรรม
-  member: Member[]; // โครงสร้างบริหารและผู้ประสานโครงการ
-  improvement_plans: ImprovementPlan[]; // แนวทางการพัฒนาโครงการจากที่ผ่านมา รวม ช่วงเตรียมงาน ช่วงดำเนินงาน ช่วงสรุปงาน
-  expected_outcomes: ExpectedOutcomes[]; // ผลที่คาดว่าจะได้รับ รวม ผู้เข้าร่วมโครงการ ผู้ปฏิบัติงาน คณะ/มหาวิทยาลัย
-  work_plans: WorkPlan[]; // แผนการดำเนินงาน รวม ช่วงเตรียมงาน ช่วงดำเนินงาน ช่วงสรุปงาน
-  actual_work_hours: number;
-  actual_volunteer_hours: number;
-  budgets: Budget[]; // งบประมาณที่ใช้ในการดำเนินงาน รวม งบจากคณะวิศวกรรมศาสตร์ งบจากสปอนเซอร์ งบอื่นๆ
-  graduate_attributes: GraduateAttribute[]; // คุณลักษณะบัณฑิตจุฬา ที่พึงประสงค์
-  type_of_activity: ActivityType;
-  sdgs: SustainableDevelopmentGoal[]; // เป้าหมายพัฒนายั่งยืน
-  tqf_standards: TQFStandard[]; // มาตรฐาน TQF กับกิจกรรมที่เกี่ยวข้อง
-  created_by: string; // user id
-  updated_by: string; // user id
+  approximateParticipants: Participant; // จำนวนผู้เข้าร่วมที่คาดว่าจะเข้าร่วมโครงการ
+  approximateWorkers: Participant; // จำนวนผู้ปฏิบัติงานที่คาดว่าจะเข้าร่วมโครงการ
+  workPlace: string;
+  activityFormat: ActivityFormat[]; // รูปแบบกิจกรรม
+  members: Member[]; // โครงสร้างบริหารและผู้ประสานโครงการ
+  improvementPlans: ImprovementPlan[]; // แนวทางการพัฒนาโครงการจากที่ผ่านมา
+  expectedOutcomes: ExpectedOutcomes[]; // ผลที่คาดว่าจะได้รับ
+  workPlans: WorkPlan[]; // แผนการดำเนินงาน
+  actualWorkHours: number;
+  actualVolunteerHours: number;
+  budgets: Budget[]; // งบประมาณที่ใช้ในการดำเนินงาน
+  graduateAttributes: GraduateAttribute[]; // คุณลักษณะบัณณิตจุณา
+  typeOfActivity: ActivityType;
+  sdgs: SDG[]; // เป้าหมายพัฒนายั่งยืน
+  tqfStandards: TQF[]; // มาตรฐาน TQF
+  createdBy: string;
+  updatedBy: string;
   createdAt: string;
   updatedAt?: string;
 }
@@ -49,10 +49,11 @@ export interface Indicator {
   readonly id: string;
   topic: string;
   measurement: string;
-  related_objective: string[]; // id of objectives
+  relatedObjective: string[]; // id of objectives
 }
 
 export interface Participant {
+  readonly id: string;
   role: 'Worker' | 'Participant';
   year1: number;
   year2: number;
@@ -74,13 +75,13 @@ export interface Member {
   role: ManagementRole;
   division?: string;
   name: string; // ชื่อจริง-นามสกุล
-  student_id: string;
+  studentId: string;
   tel: string;
 }
 
 export interface ImprovementPlan {
   readonly id: string;
-  phase: 'Preparation' | 'Execution' | 'Conclusion';
+  phase: PlanPhase;
   details: ImprovementItem[];
 }
 
@@ -98,15 +99,15 @@ export interface ExpectedOutcomes {
 
 export interface WorkPlan {
   readonly id: string;
-  phase: 'Preparation' | 'Execution' | 'Conclusion';
+  phase: PlanPhase;
   details :WorkPlanItem[];
 }
 
 export interface WorkPlanItem {
   readonly id: string;
   detail: string;
-  start_date: string;
-  end_date: string;
+  startDate: string;
+  endDate: string;
 }
 
 export interface Budget {
@@ -122,7 +123,7 @@ interface BaseBudgetDeatil {
   name: string;
   amount: number;
   unit: string;
-  cost_per_unit: number;
+  costPerUnit: number;
 }
 
 export interface BudgetType1 extends BaseBudgetDeatil {
@@ -131,28 +132,28 @@ export interface BudgetType1 extends BaseBudgetDeatil {
 
 export interface BudgetType2 extends BaseBudgetDeatil {
   type: 'Type 2';
-  number_of_days: number;
-  day_unit: string;
+  numberOfDays: number;
+  dayUnit: string;
 }
 
 export interface GraduateAttribute {
   readonly id: string;
   attribute: GraduateAttributeType;
   question: ObjectiveType;
-  response_leader: boolean;
-  response_worker: boolean;
-  response_attendee: boolean;
-  activity_id: string[]; // id of activities
+  responseLeader: boolean;
+  responseWorker: boolean;
+  responseAttendee: boolean;
+  activityId: string[]; // id of activities
 }
 
-export interface SustainableDevelopmentGoal {
+export interface SDG {
   readonly id: string;
-  sdgs_goal: SustainableDevelopmentGoalType;
-  activity_id: string[]; // id of activities
+  sdgGoal: SDGType;
+  activityId: string[]; // id of activities
 }
 
-export interface TQFStandard { // จับคู่กิจกรรมกับมาตรฐาน TQF
+export interface TQF { // จับคู่กิจกรรมกับมาตรฐาน TQF
   readonly id: string;
-  tqf_standards: TQFStandardType;
-  activity_id: string[]; // id of activities
+  tqfStandard: TQFType;
+  activityId: string[]; // id of activities
 }
