b1457cf04e945af2e49b89ace2d36b79a32708dd
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2025-04-14 14:54:39 +0700

    feat: add DocumentTypeOneSchema and DocumentTypeZero interface

---
+++ apps/web/src/interface/document/document-type-two-zod.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import {FilingType} from "@/src/constant/enum";
+import { BudgetSchema } from "./document-type-zero-zod";
+
+export const DocumentTypeOneSchema = z.object({
+  id: z.string().readonly(),
+  projectCode: z.string(),
+  projectNameTH: z.string(),
+  projectNameEN: z.string(),
+  filingType: z.nativeEnum(FilingType),
+  filingCode: z.string(),
+  filingName: z.string(),
+  department: z.string().nullable(),
+  isOneDay: z.boolean(),
+  startDate: z.string(),
+  endDate: z.string().optional(),
+  objectiveSummarizeDetail: z.string(),
+
+  budgets: z.array(BudgetSchema),
+  procurementBudgets: z.number().positive(),
+
+  createdBy: z.string(),
+  updatedBy: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string().optional(),
+})
---
+++ apps/web/src/interface/document/document-type-two.ts
@@ -0,0 +1,26 @@
+import type {FilingType} from "@/src/constant/enum";
+import type { Budget } from "./document-type-zero";
+
+
+export interface DocumentTypeZero { // เอกสารเปิดโครง
+  readonly id: string;
+  projectCode: string;
+  projectNameTH: string;
+  projectNameEN: string;
+  filingType: FilingType;
+  filingCode: string;
+  filingName: string;
+  department: string | null; // esc fields, club, null
+  isOneDay: boolean;
+  startDate: string;
+  endDate?: string; // For projects lasting more than 1 day
+  objectiveSummarizeDetail: string; // วัตถุประสงค์โครงการแบบย่อ
+
+  budgets: Budget[]; // งบประมาณที่ใช้ในการดำเนินงาน
+  procurementBudgets: number;
+
+  createdBy: string;
+  updatedBy: string;
+  createdAt: string;
+  updatedAt?: string;
+}
